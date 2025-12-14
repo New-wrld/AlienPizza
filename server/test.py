aa21eb94b10969bed828506a4e64db151ccdd19d
ketr4x <ketraxpl@gmail.com>
2025-12-14 09:10:37 -0500

    Complete the migration to FastAPI

--- server/test.py
+++ server/test.py
@@ -1,12 +1,12 @@
 import requests
 
-url = 'http://127.0.0.1:5000/api/evaluate'
+url = 'http://127.0.0.1:8000/api/evaluate'
 payload = {"toppings": ["smth1", "smth2"]}
 
 response = requests.post(url, json=payload)
 print(response.text)
 
-url = 'http://127.0.0.1:5000/api/toppings'
+url = 'http://127.0.0.1:8000/api/toppings'
 
 response = requests.get(url)
 print(response.text)
